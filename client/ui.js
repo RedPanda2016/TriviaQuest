f2a1c0e76df3abe5f447064d650925e25ae73bb8
Charleen Fei <fei.charleen@gmail.com>
2016-12-20 03:17:45 +0100

    Update ui.js

--- client/ui.js
+++ client/ui.js
@@ -3,7 +3,7 @@
  */
 $( function() {
     $( "#draggable" ).draggable().css({'height':60, 'width':80, 'top': 45, 'left' : 20});
-    $( "#jukebox" ).droppable({
+    $( ".jukebox" ).droppable({
         drop: function() {
             $("#draggable").css({'height':80, 'width':80}).hide("drop", {direction: "down"}, "slow", function(){
                 $(this).css({'height':60, 'width':80, 'top': 45, 'left' : 20}).fadeIn()
